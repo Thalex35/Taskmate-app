6ed426b4d1fd4c9397b5214ebd415ffbc6e54c8c
Pressaille Rosar <pressaillerosar@gmail.com>
2026-05-01 09:03:31 -0700

    ..

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,33 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
 import { Sun, Moon } from "lucide-react";
 import "../../styles/NavBar.css"
 
 
 export default function NavBar() {
 
-import { User } from "lucide-react";
-
-import "../../styles/NavBar.css";
-
-
-export default function NavBar({
-  ShowAddBtn = true,
-  showLogout = false,
-  showMode = true,
-}) {
-  const [modeSombre, setModeSombre] = useState(false);
+  const[modeSombre, setModeSombre] = useState(false);
 
   return (
     <nav className="navbar">
-      <Link to="/dashboard" className="navbar-logo">
-        <span className="navbar-logo-task">Task</span>
-        <span className="navbar-logo-mate">Mate</span>
+      <Link to="/dashboard"
+      className="navbar-logo">
+        <span className="logo-task">Task</span>
       </Link>
 
-      <div className="navbar-action">
 
+
+      <div className="navbar-action">
         <button className="navbar-btn-mode"
           onClick={() => setModeSombre
           (!modeSombre)}>
@@ -39,31 +29,9 @@ export default function NavBar({
         <Link to= "/devoir/nouveau"
         className="navbar-btn-nouveau">
           + Nouveau Devoir
-
-        {showMode && (
-          <button
-            className="navbar-btn-mode"
-            onClick={() => setModeSombre(!modeSombre)}
-          >
-            {modeSombre ? "☀️ Mode clair" : "🌙 Mode sombre"}
-          </button>
-        )}
-
-        {showLogout ? (
-          <Link to="/login" className="navbar-btn-logout">
-            Déconnexion
-          </Link>
-        ) : ShowAddBtn ? (
-          <Link to="/new-devoir" className="navbar-btn-nouveau">
-            + Nouveau Devoir
-          </Link>
-        ) : null}
-
-        <Link to="/profile" className="navbar-btn-profile">
-          <User size={24} color="white" />
-
         </Link>
       </div>
+      
     </nav>
   );
 }
