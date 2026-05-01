c7bce60fccbf348f42bb9ffbcd4c6a18d8acf315
Theodore <theodore@email.com>
2026-05-01 11:47:43 -0400

    final css

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,14 @@ export default function Profile() {
   return (
     <div>
       <h1>Profil</h1>
+      <div>
+        <div className="menu-avatar">
+          {user?.user_metadata?.first_name?.charAt(0) +
+            user?.user_metadata?.last_name?.charAt(0) || "U"}
+        </div>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
